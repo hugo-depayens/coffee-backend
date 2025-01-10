59267715cf70cae9edf8088b8149091f6e643837
Zohir <vasv29834@gmail.com>
2025-01-10 09:33:51 +0500

    fix: swagger-fix

--- config/generateSwagger.js
+++ config/generateSwagger.js
@@ -1,7 +1,7 @@
 import swaggerAutogen from 'swagger-autogen';
 
-const outputFile = './swagger-output.json'; // Путь к выходному файлу JSON
-const endpointsFiles = ['../routes/*.js']; // Путь к файлам с маршрутами
+const outputFile = './swagger-output-cart.json'; // Путь к выходному файлу JSON
+const endpointsFiles = ['../routes/cartRouter.js']; // Путь к файлам с маршрутами
 
 swaggerAutogen(outputFile, endpointsFiles).then(() => {
     import('../src/app.js');
